5d943b576c173a907c0edabedf91dd2d4def3f1c
scalesan <scalesan@msu.edu>
2017-10-27 09:28:17 -0400

    New Files and modifications

---
+++ Desktop/Practice Coding/PHP and MySQL/challenge.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Challenge</title>
+</head>
+<body>
+<form action = "challengeprocess.php" method="post">
+	<input type="text" name="firstname" placeholder="Please enter your first name">
+	<input type="text" name="lastname" placeholder="Please enter your last name">
+	<br>
+	<br>
+	<input type="submit" name="submit">
+
+</form>
+</body>
+</html>
