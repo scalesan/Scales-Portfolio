64c79ca6e2e46cf1d37c36d9c705f40374d2abdc
scalesan <scalesan@msu.edu>
2017-11-01 15:37:07 -0400

    Put wrong file

--- js/restaurant.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	var img = $('img[id^="content-"]').hide(), i=0;
-	(function cycle(){
-		img.eq(i).fadeIn(500).delay(4000).fadeOut(1, cycle);
-		i = ++i % img.length;
-})();	
-})
-
-
