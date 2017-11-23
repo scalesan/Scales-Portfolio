c3c999bf7e2550f081558be8926c3b00e056ea9a
scalesan <scalesan@msu.edu>
2017-11-23 08:58:57 -0500

    New Features-Video and Utility

--- Desktop/Angular Coding/ngSrvup/client/src/app/video-detail/video-detail.component.ts
+++ Desktop/Angular Coding/ngSrvup/client/src/app/video-detail/video-detail.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
+//Work with Route, gets parameters from route
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-video-detail',
   templateUrl: './video-detail.component.html',
   styleUrls: ['./video-detail.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class VideoDetailComponent implements OnInit {
+export class VideoDetailComponent implements OnInit, OnDestroy {
+	private routeSub:any;
+	slug:string;
 
-  constructor() { }
+
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+  	this.routeSub = this.route.params.subscribe(params=>{
+  		this.slug = params['slug']
+  	})
+  }
+  ngOnDestroy(){
+  	this.routeSub.unsubscribe()
   }
 
 }
--- Desktop/Angular Coding/ngSrvup/client/src/app/video-list/video-list.component.ts
+++ Desktop/Angular Coding/ngSrvup/client/src/app/video-list/video-list.component.ts
@@ -9,23 +9,33 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 export class VideoListComponent implements OnInit {
 	title="Video List"
 	//videoList = ["Item 1", "Item 2", "Item 3"] JSON
+	todayDate;
 	videoList = [
 		{
 		name: "Item 1",
-		slug: "Item-1"
+		slug: "item-1",
+		embed:`1hyjLD7pk10`,
 		}, 
 		{
 		name: "Item 2",
-		slug: "Item-2"
+		slug: "item-2",
+		embed:`1hyjLD7pk10`,
 		}, 
 		{
 		name: "Item 3",
-		slug: "Item-3"
-		}, 
+		slug: "item-3",
+		embed:"",
+		}
 	]
   constructor() { }
 
   ngOnInit() {
+  	this.todayDate = new Date
+  }
+
+  getEmbedUrl(item){
+  	return 'https://www.youtube.com/embed/' + item.embed + '?ecver=2'
+
   }
 
 }
