a16dc4168808bc66976f5ed29e24eee8f1b04ee7
scalesan <scalesan@msu.edu>
2017-11-23 08:54:12 -0500

    Changes to Angular Coding

--- Desktop/Practice Coding/Restaurant/contact.php
+++ Desktop/Practice Coding/Restaurant/contact.php
@@ -26,6 +26,8 @@ consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </p>
+<br>
+<br>
 <form action="formprocess.php" method="post">
   <label>First Name: </label>
   <input type="text" name="firstName"><br><br>
@@ -38,6 +40,12 @@ proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   <label id="commentLabel">Question/Comment: </label>
   <input type="text" name="comment" class="commentProperties">
 </form>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>
 </body>
 </html>
